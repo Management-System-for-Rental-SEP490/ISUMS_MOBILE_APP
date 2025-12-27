cffb44f4891e848bcbc4e4701ad4e75940162450
Anh-Khoa-fpt <khoanqase184731@fpt.edu.vn>
2025-12-27 19:01:42 +0700

    update icon

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Icons from './src/theme/icon';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>my capstone project</Text>
+      <View style={{flexDirection: 'row', gap: 10}}>
+      <Icons.home />
+      <Icons.user />
+      <Icons.menu />
+      <Icons.water />
+      <Icons.electric />
+      <Icons.contract />
+      <Icons.brain />
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/theme/icon.tsx
@@ -0,0 +1,36 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+type IconProps = {
+  size?: number;
+  color?: string;
+};
+
+const Icons = {
+  
+  home: ({ size = 24, color = 'black' }: IconProps = {}) => (
+    <AntDesign name="home" size={size} color={color} />
+  ),
+  user: ({ size = 24, color = 'black' }: IconProps = {}) => (
+    <FontAwesome name="user" size={size} color={color} />
+  ),
+  menu:({ size = 24, color = 'black' }: IconProps = {}) => (
+    <AntDesign name="menu" size={size} color={color} />
+  ),
+  water:({ size = 24, color = 'black' }: IconProps = {}) => (
+    <FontAwesome5 name="hand-holding-water" size={size} color={color} />
+  ),
+  electric:({ size = 24, color = 'black' }: IconProps = {}) => (
+    <MaterialIcons name="electrical-services" size={size} color={color} />
+  ),
+  contract:({ size = 24, color = 'black' }: IconProps = {}) => (
+    <FontAwesome5 name="file-contract" size={size} color={color} />
+  ),
+  brain:({ size = 24, color = 'black' }: IconProps = {}) => (
+    <FontAwesome5 name="brain" size={size} color={color} />
+  ),
+};
+
+export default Icons;
